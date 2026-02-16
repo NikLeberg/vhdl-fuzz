fb0b0b9a8c09e7880223e661b2a19503523608f3
NikLeberg <39563554+NikLeberg@users.noreply.github.com>
2026-02-16 21:44:30 +0000

    src: implement differential fuzzer

--- src/fuzz.cc
+++ src/fuzz.cc
@@ -1,16 +1,110 @@
 // SPDX-License-Identifier: MIT
 
-#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <cassert>
+#include <filesystem>
+
 #include <libfuzzer/libfuzzer_macro.h>
 
 #include "fuzz.h"
 
+Fuzz::Fuzz() : gen_() {
+    char *val = std::getenv(ENV_KEY_FORCE_FILEGEN.c_str());
+    force_filegen_ = (val != NULL && *val != '0');
+}
+
 void Fuzz::run_once_for_proto(const DesignFile &proto) {
-    std::string vhdl = gen_.generate_vhdl_from_proto(proto);
-    if (!vhdl.empty()) {
-        std::cout << vhdl;
-        // ToDo: call into targets to fuzz i.e. GHDL and NVC.
+    std::string vhdl;
+    try {
+       vhdl = gen_.generate_vhdl_from_proto(proto);
+    } catch (const std::invalid_argument &e) {
+        // ignore malformed proto message
+        return;
     }
+
+    std::size_t hash = hash_of_strbuf(vhdl);
+    std::filesystem::path path = strbuf_to_tmpfile(vhdl, hash);
+
+    bool nvc_ok = analyze_with_nvc(path, hash);
+    bool ghdl_ok = analyze_with_ghdl(path, hash);
+
+    if (nvc_ok == ghdl_ok) {
+        if (force_filegen_) {
+            std::cout << "==========" << std::endl;
+            path = move_to_currdir(path);
+            std::cout << "Saved input file as: " << path << std::endl;
+        } else {
+            delete_file(path);
+        }
+        return;
+    }
+
+    std::cout << "==========" << std::endl;
+    std::cout << "Analyze missmatch: NVC " << (nvc_ok ? "PASSED" : "FAILED")
+        << ", but GHDL " << (ghdl_ok ? "PASSED" : "FAILED") << std::endl;
+
+    std::cout << "==========" << std::endl;
+    std::cout << "Output of NVC:" << std::endl;
+    analyze_with_nvc(path, hash, false);
+    std::cout << "==========" << std::endl;
+    std::cout << "Output of GHDL:" << std::endl;
+    analyze_with_ghdl(path, hash, false);
+
+    std::cout << "==========" << std::endl;
+    path = move_to_currdir(path);
+    std::cout << "Saved input file as: " << path << std::endl;
+
+    assert(false && "Mismatch found between NVC and GHDL");
+}
+
+bool Fuzz::analyze_with_nvc(const std::filesystem::path &path, const std::size_t hash, const bool silent) {
+    (void)hash;
+    std::ostringstream cmd;
+    cmd << "nvc --std=19 -a --no-save " << path
+        << " < /dev/null"
+        << (silent ? " > /dev/null 2>&1" : "");
+    int result = system(cmd.str().c_str());
+    
+    return (result == 0);
+}
+
+bool Fuzz::analyze_with_ghdl(const std::filesystem::path &path, const std::size_t hash, const bool silent) {
+    std::string shash = std::to_string(hash);
+    std::ostringstream cmd;
+    cmd << "ghdl -a --std=08 --work=" << shash
+        << " --workdir=" << TMPDIR << " " << path
+        << " < /dev/null"
+        << (silent ? " > /dev/null 2>&1" : "");
+    int result = system(cmd.str().c_str());
+    delete_file(TMPDIR / (shash + "-obj08.cf"));
+
+    return (result == 0);
+}
+
+std::size_t Fuzz::hash_of_strbuf(const std::string &str) {
+    return hasher_(str);
+}
+
+std::filesystem::path Fuzz::strbuf_to_tmpfile(const std::string &str, const std::size_t hash) {
+    std::filesystem::path path = TMPDIR / ("fuzz" + std::to_string(hash));
+    std::ofstream outfile(path);
+    outfile << str;
+    outfile.close();
+
+    return path;
+}
+
+void Fuzz::delete_file(const std::filesystem::path &path) {
+    std::filesystem::remove(path);
+}
+
+std::filesystem::path Fuzz::move_to_currdir(const std::filesystem::path &path) {
+    std::filesystem::path npath = std::filesystem::current_path() / path.filename();
+    std::filesystem::copy(path, npath, std::filesystem::copy_options::update_existing);
+    std::filesystem::remove(path);
+    return npath;
 }
 
 DEFINE_PROTO_FUZZER(const DesignFile &proto) {
--- src/fuzz.h
+++ src/fuzz.h
@@ -2,18 +2,31 @@
 
 #pragma once
 
-#include "gen.h"
-#include "vhdl2019.pb.h"
+#include "gen08.h"
+#include "vhdl2008.pb.h"
+
+using namespace VHDL2008;
 
 class Fuzz {
   public:
-    Fuzz() : gen_() {
-        // ...
-    }
+    Fuzz();
     ~Fuzz() = default;
 
     void run_once_for_proto(const DesignFile &proto);
 
+    bool analyze_with_nvc(const std::filesystem::path &path, const std::size_t hash, const bool silent = true);
+    bool analyze_with_ghdl(const std::filesystem::path &path, const std::size_t hash, const bool silent = true);
+
+    std::size_t hash_of_strbuf(const std::string &str);
+    std::filesystem::path strbuf_to_tmpfile(const std::string &str, const std::size_t hash);
+    void delete_file(const std::filesystem::path &path);
+    std::filesystem::path move_to_currdir(const std::filesystem::path &path);
+
   private:
+    const std::filesystem::path TMPDIR = "/dev/shm";
+    const std::string ENV_KEY_FORCE_FILEGEN = "FUZZ_FORCE_FILEGEN";
+
     Gen gen_;
+    std::hash<std::string> hasher_;
+    bool force_filegen_ = false;
 };
